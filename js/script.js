2dabd94649cd7f40407510c3d52dc26dd82aa3b8
student-apnacolg <nik70655@gmail.com>
2025-07-20 14:59:19 +0530

    .

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const toogleBtn = document.querySelector('.toggleBtn');
+const toogleBtnIcon = document.querySelector('.toggleBtn i');
+const dropdownMenu = document.querySelector('.dropdownMenu');
+
+toogleBtn.onclick = () => {
+  dropdownMenu.classList.toggle('open');
+  const isOpen = dropdownMenu.classList.contains('open');
+
+  toogleBtnIcon.classList = isOpen ? "fa-solid fa-xmark" : "fa-solid fa-bars";
+}
+
+
